71852b9a42757854915b93a609d933d93021af4b
Ashkan <ashkanebtekari@gmail.com>
2020-10-01 18:50:24 +0330

    Updated and Documented code with more details

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -4,31 +4,47 @@ import 'package:args/args.dart';
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
 
-// For Google Cloud Run, set _hostname to '0.0.0.0'.
+// HostName & IP Configuration
+
 const _hostname = 'localhost';
 
-void main(List<String> args) async {
-  var parser = ArgParser()..addOption('port', abbr: 'p');
-  var result = parser.parse(args);
+// Starting The Server
 
-  // For Google Cloud Run, we respect the PORT environment variable
-  var portStr = result['port'] ?? Platform.environment['PORT'] ?? '8080';
-  var port = int.tryParse(portStr);
+void main(List<String> args) async {
+  // Initializing ...
+
+  var init = ArgParser()..addOption('port', abbr: 'p');
+  var parser = init.parse(args);
+
+  // Port Configurations
+
+  var portvalue = parser['port'] ?? Platform.environment['PORT'] ?? '8080';
+  var port = int.tryParse(portvalue);
+
+  // Port Configuration Fail
 
   if (port == null) {
-    stdout.writeln('Could not parse port value "$portStr" into a number.');
-    // 64: command line usage error
+    stdout.writeln('Could not parse port value "$portvalue" into a number.');
+
+    // ExitCode 64 For Command Line Usage Err
+
     exitCode = 64;
     return;
   }
+
+  // Handeling Log Requests
 
   var handler = const shelf.Pipeline()
       .addMiddleware(shelf.logRequests())
       .addHandler(_echoRequest);
 
+  // Setting Configured Credentials To The Server ...
+
   var server = await io.serve(handler, _hostname, port);
   print('Serving at http://${server.address.host}:${server.port}');
 }
+
+// Request Logger
 
 shelf.Response _echoRequest(shelf.Request request) =>
     shelf.Response.ok('Request for "${request.url}"');
